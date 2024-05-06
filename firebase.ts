f82e1a15aacfebe649375b48ed51ce6d798ddbdd
ydideh810 <yd.baller810@gmail.com>
2024-05-05 20:00:18 -0400

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -8,12 +8,12 @@ import { getFunctions } from 'firebase/functions'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: "AIzaSyBkxVwmkL8WV_eRpSZHoWj2zGW4q0U6hf4",
+  authDomain: "mapwrld-1.firebaseapp.com",
+  projectId: "mapwrld-1",
+  storageBucket: "mapwrld-1.appspot.com",
+  messagingSenderId: "611533888292",
+  appId: "1:611533888292:web:68e91f1a88fb4684ce31b1",
 }
 
 // Initialize Firebase
